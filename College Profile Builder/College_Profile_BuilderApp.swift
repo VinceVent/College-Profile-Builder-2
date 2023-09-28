87db60b50c322f670c5b351ba3ed7a6d6cf274f3
VinceVent <Vchen4070@stu.d214.org>
2023-09-28 10:51:28 -0500

    Initial Commit

---
+++ College Profile Builder/College_Profile_BuilderApp.swift
@@ -0,0 +1,17 @@
+//
+//  College_Profile_BuilderApp.swift
+//  College Profile Builder
+//
+//  Created by vincent chen on 9/28/23.
+//
+
+import SwiftUI
+
+@main
+struct College_Profile_BuilderApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
